6731c676dd3ccc0dc82fab8a8e431fee73fc3048
Aiden Samuel <65487599+aiden506@users.noreply.github.com>
2022-02-02 20:17:16 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+path=r'C:\\Users\\AIDEN SAMUEL\\PycharmProjects\\Pythonproject1\model_churn.pkl'
+model = pickle.load(open(path, 'rb'))
+#Churn_pred
+@app.route('/')
+def home():
+    return render_template('Churn_pred.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    if output>0.5:
+        output='A Churn case'
+    else:
+        output='Not A Churn case'
+    return render_template('Churn_pred.html', prediction_text='This case is  {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
